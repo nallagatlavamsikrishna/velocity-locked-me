100ed21553e78623aef0dd2d8820f03f6362cedd
Phani <nallagatlavamsikrishna23@gmail.com>
2022-03-31 21:41:27 +0530

    updated

---
+++ src/com/lockedme/LockedMeMain.java
@@ -0,0 +1,15 @@
+package com.lockedme;
+
+public class LockedMeMain {
+
+	public static void main(String[] args) {
+		// Create "main" folder if not present in current folder structure
+		FileOperations.createMainFolderIfNotPresent("main");
+		
+		MenuOptions.printWelcomeScreen("LockedMe", "Yogeshwari Patil");
+		
+		HandleOptions.handleWelcomeScreenInput();
+
+	}
+
+}
